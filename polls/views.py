581e8486ee0ba6fd0af989210cc666ae2a245540
lamborghini1993 <1323242382@qq.com>
2019-08-28 20:19:22 +0800

    3-编写更多视图

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at quesiont %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at result of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on quesion %s." % question_id)
