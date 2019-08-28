760f7a046348f5c35f05d1d4d6ee84824a616916
lamborghini1993 <1323242382@qq.com>
2019-08-28 21:10:36 +0800

    3-一个快捷函数： render()

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
+
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
+    # 一个快捷函数： render()
+    return render(request, "polls/index.html", context)
+
+    # 载入模板，填充上下文，再返回由它生成的 HttpResponse 对象
+    template = loader.get_template("polls/index.html")
     return HttpResponse(template.render(context, request))
 
 
