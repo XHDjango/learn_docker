48e20b7617d08d0dfbad605b1cb5dd0eb95fcf40
lamborghini1993 <1323242382@qq.com>
2019-08-29 11:47:57 +0800

    4-改良视图+改良 URLconf

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,28 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
-
+from django.views import generic
 from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        return latest_question_list
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def index(request):
