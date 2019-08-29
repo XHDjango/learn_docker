08155912939b992461eaaafa756ac292b0e7ff36
lamborghini1993 <1323242382@qq.com>
2019-08-29 11:03:54 +0800

    3-使用模板系统 detail

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 from .models import Question
@@ -19,7 +19,16 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at quesiont %s." % question_id)
+    # 一个快捷函数： get_object_or_404()
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+    # 如果指定问题 ID 所对应的问题不存在，这个视图就会抛出一个 Http404 异常。
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
