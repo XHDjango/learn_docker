0ff15ba1c76874b893912da459b1fa086a1bf7a1
lamborghini1993 <1323242382@qq.com>
2019-08-29 15:05:16 +0800

    7-自定义后台表单（显示顺序）

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]  # 作用域，以及前台显示顺序
+
+
 # 向管理页面中加入投票应用
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
