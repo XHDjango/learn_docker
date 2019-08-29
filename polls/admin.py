1d852eec585b2168f46db20090330c592d2abc65
lamborghini1993 <1323242382@qq.com>
2019-08-29 15:29:40 +0800

    7-自定义后台更改列表

--- polls/admin.py
+++ polls/admin.py
@@ -15,6 +15,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 # 向管理页面中加入投票应用
