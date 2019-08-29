5fdb691fad1c50d3923be355c8e94650761980f1
lamborghini1993 <1323242382@qq.com>
2019-08-29 15:12:07 +0800

    7-自定义后台表单-添加字段集的标题

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,11 @@ from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]  # 作用域，以及前台显示顺序
-
+    # fieldsets 元组中的第一个元素是字段集的标题
+    fieldsets = [
+        (None, {"fields":["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 # 向管理页面中加入投票应用
 admin.site.register(Question, QuestionAdmin)
