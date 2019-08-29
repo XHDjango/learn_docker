01fe78a79bc96906ade45dfdef0f2b3414663f42
lamborghini1993 <1323242382@qq.com>
2019-08-29 15:23:08 +0800

    7-添加关联的对象

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from .models import Question
+
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     # fieldsets 元组中的第一个元素是字段集的标题
     fieldsets = [
-        (None, {"fields":["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+
 
 # 向管理页面中加入投票应用
 admin.site.register(Question, QuestionAdmin)
