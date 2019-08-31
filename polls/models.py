61f35ab9b63e0ed937c4d577b588d6cd761c1bf2
duoyi-window <1323242382@qq.com>
2019-08-31 17:02:02 +0800

    2 str-fix

--- polls/models.py
+++ polls/models.py
@@ -21,9 +21,9 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    quesion = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.quesion
+        return self.choice_text
